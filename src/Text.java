462a547ae6a84c2d055f79f03c86b02d6445b25d
srihari <srihariadada19978@gmail.com>
2024-10-14 16:20:41 +0530

    commiting to hello world

--- src/Text.java
+++ src/Text.java
@@ -3,6 +3,7 @@ public class Text {
 	public static void main(String[] args) {
 		System.out.println("Text");
 		System.out.println("adding code from terminal");
+		System.out.println("Hello Text");
 	}
 
 }
