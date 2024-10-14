c2c0c1cb1dba5a0e6c112b1219a4b5493bf9a57e
srihari <srihariadada19978@gmail.com>
2024-10-14 15:21:06 +0530

    final changes

--- src/Text.java
+++ src/Text.java
@@ -2,7 +2,10 @@
 public class Text {
 	public static void main(String[] args) {
 		System.out.println("Text");
+
 		System.out.println("adding code from terminal");
+		System.out.println();
+
 	}
 
 }
